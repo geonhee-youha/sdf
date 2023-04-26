88072e0e4dd76673348cb7d7bd36a3b1de8c5bad
geonhee-youha <geonhee.lee@youha.info>
2023-04-26 14:03:04 +0900

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { Box } from "@mui/material";
+import Button from "../components/atoms/Button";
+
+export default function Index() {
+  return (
+    <>
+      <Box
+        sx={{
+          maxWidth: "640px",
+          m: `0 auto`,
+          "& img": {
+            width: "100%",
+            height: "auto",
+          },
+          p: `0 0 100px 0`,
+        }}
+      >
+        <img src="/images/poster.png" />
+      </Box>
+      <Box
+        sx={{
+          position: "fixed",
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: "640px",
+            m: `0 auto`,
+            p: `8px 16px`,
+          }}
+        >
+          <Button fullWidth backgroundColor="#0ED5BC" size='lg'>지금 참가신청하기</Button>
+        </Box>
+      </Box>
+    </>
+  );
+}
